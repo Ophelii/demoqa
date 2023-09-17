aa273387b75abba29d0f41e3579c1116c9df9e44
Elena <bellemortt@gmail.com>
2023-09-17 21:32:38 +0300

    up with homework from 15/09/23

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -5,27 +5,12 @@ from pages.demoqa import DemoQA
 def test_icon_exist(browser):
     demo_qa_page = DemoQA(browser)
     demo_qa_page.visit()
-    demo_qa_page.click_on_the_icon()
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.icon.exist()
 
 
 
 
 
 
-
-
-
-# from conftest import browser
-# from pages.demoqa import DemoQA
-
-
-# def test_icon_demoqa(browser):
-#     browser.get("https://demoqa.com/")
-#     icon = browser.find_element(By.CSS_SELECTOR, "#app > header > a")
-#
-#     if icon is None:
-#         print('Элемент не найден')
-#     else:
-#         print('Элемент найден')
---
+++ tests/tests_hw/test_check_text.py
@@ -0,0 +1,13 @@
+from conftest import browser
+from pages.elements_page import ElementsPage
+from pages.demoqa import DemoQA
+
+
+def test_page_elements(browser):
+    demo_qa_page = DemoQA(browser)
+    elements_page = ElementsPage(browser)
+    demo_qa_page.visit()
+    assert demo_qa_page.footer_text.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+    demo_qa_page.btn_elements.click()
+    assert elements_page.element_center_text.get_text() == 'Please select an item from left to start practice.'
+
